a0718d6a203aeee928837b972f8bb3ee6d1dfce8
Codie@123 <elliott6518@gmail.com>
2024-07-22 14:35:37 +0530

    Update EnglishQuiz.jsx

--- src/component/EnglishQuiz.jsx
+++ src/component/EnglishQuiz.jsx
@@ -10,7 +10,7 @@ const EnglishQuiz = () => {
         <h1 className="text-white font-extrabold text-9xl mb-6">
           Choose A Question
         </h1>
-        <div className=" max-w-screen-md w-full grid grid-cols-2 gap-4   backdrop-blur-2xl bg-white/30 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
+        <div className=" max-w-screen-2xl w-full grid grid-cols-2 gap-4   backdrop-blur-2xl bg-white/30 border border-gray-200 dark:border-gray-700 rounded-lg p-8 md:p-12 mb-8">
           {questionData.map((question, index) => (
             <Link key={index} className="" to={`/quiz/${index}`}>
               <button
